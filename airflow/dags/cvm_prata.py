916715fe43f2104b4189849bc4dd775065b91568
alcantarasc <alcantarasc@gmail.com>
2025-07-16 20:19:12 -0300

    faz a dag ser incremental

--- airflow/dags/cvm_prata.py
+++ airflow/dags/cvm_prata.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine, text
 from airflow.models import Variable
 from datetime import datetime, timedelta
 import logging
+import time
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -105,40 +106,53 @@ def salvar_ranking_movimentacao():
         # Limpa dados antigos
         connection.execute(text("DELETE FROM RANKING_MOVIMENTACAO"))
         
-        # Query para obter o ranking de movimentação
+        # Query otimizada para obter o ranking de movimentação
         query = text("""
+            WITH movimentacao_agregada AS (
+                SELECT 
+                    i.cnpj_fundo_classe,
+                    i.tp_fundo_classe,
+                    r.denominacao_social,
+                    SUM(i.resg_dia) as total_resgates,
+                    SUM(i.captc_dia) as total_aportes,
+                    SUM(i.captc_dia - i.resg_dia) as fluxo_liquido,
+                    MAX(i.vl_total) as vl_total  -- Pega o valor mais recente
+                FROM INFORMACAO_DIARIA i
+                LEFT JOIN REGISTRO_FUNDO r ON i.cnpj_fundo_classe = r.cnpj_fundo
+                WHERE i.dt_comptc BETWEEN :data_inicio AND :data_fim
+                  AND (i.resg_dia IS NOT NULL OR i.captc_dia IS NOT NULL)
+                  AND i.vl_total > 0  -- Filtra apenas fundos com patrimônio
+                GROUP BY i.cnpj_fundo_classe, i.tp_fundo_classe, r.denominacao_social
+                HAVING SUM(i.captc_dia - i.resg_dia) != 0
+            )
             INSERT INTO RANKING_MOVIMENTACAO (
                 cnpj_fundo_classe, tp_fundo_classe, dt_comptc, denominacao_social,
                 vl_total, total_resgates, total_aportes, fluxo_liquido,
                 percentual_fluxo_liquido, ranking
             )
             SELECT 
-                i.cnpj_fundo_classe,
-                i.tp_fundo_classe,
-                i.dt_comptc,
-                r.denominacao_social,
-                i.vl_total,
-                SUM(i.resg_dia) as total_resgates,
-                SUM(i.captc_dia) as total_aportes,
-                SUM(i.captc_dia - i.resg_dia) as fluxo_liquido,
+                cnpj_fundo_classe,
+                tp_fundo_classe,
+                :ultima_data as dt_comptc,  -- Usa a data mais recente
+                denominacao_social,
+                vl_total,
+                total_resgates,
+                total_aportes,
+                fluxo_liquido,
                 CASE 
-                    WHEN i.vl_total > 0 THEN 
-                        (SUM(i.captc_dia - i.resg_dia) / i.vl_total) * 100
+                    WHEN vl_total > 0 THEN 
+                        (fluxo_liquido / vl_total) * 100
                     ELSE 0 
                 END as percentual_fluxo_liquido,
-                ROW_NUMBER() OVER (ORDER BY SUM(i.captc_dia - i.resg_dia) DESC) as ranking
-            FROM INFORMACAO_DIARIA i
-            LEFT JOIN REGISTRO_FUNDO r ON i.cnpj_fundo_classe = r.cnpj_fundo
-            WHERE i.dt_comptc BETWEEN :data_inicio AND :data_fim
-              AND (i.resg_dia IS NOT NULL OR i.captc_dia IS NOT NULL)
-            GROUP BY i.cnpj_fundo_classe, i.tp_fundo_classe, r.denominacao_social, i.dt_comptc, i.vl_total
-            HAVING SUM(i.captc_dia - i.resg_dia) != 0
+                ROW_NUMBER() OVER (ORDER BY fluxo_liquido DESC) as ranking
+            FROM movimentacao_agregada
             ORDER BY fluxo_liquido DESC
         """)
         
         connection.execute(query, {
             'data_inicio': data_inicio,
-            'data_fim': ultima_data
+            'data_fim': ultima_data,
+            'ultima_data': ultima_data
         })
         logger.info(f"Ranking de movimentação salvo com sucesso para o período {data_inicio} a {ultima_data}")
 
