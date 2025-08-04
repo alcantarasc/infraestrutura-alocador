16ef818f4a93e846637fe77c55f7606324b080de
alcantarasc <alcantarasc@gmail.com>
2025-08-04 10:53:38 -0300

    Update settings.py

--- app/rest/settings.py
+++ app/rest/settings.py
@@ -6,10 +6,10 @@ import os
 DATABASE_PROVIDER = os.environ.get('DATABASE_PROVIDER', 'postgresql')
 DATABASE_ADAPTER = os.environ.get('DATABASE_ADAPTER', 'psycopg2')
 DATABASE_NAME = os.environ.get('DATABASE_NAME', 'screening_cvm')
-DATABASE_USER = os.environ.get('DATABASE_USER', 'lucas')
-DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD', 'kappa123test')
-DATABASE_HOST = os.environ.get('DATABASE_HOST', '167.88.33.95')
-DATABASE_PORT = os.environ.get('DATABASE_PORT', '5432')
+DATABASE_USER = os.environ.get('DATABASE_USER', 'DATABASE_USER')
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD', 'DATABASE_PASSWORD')
+DATABASE_HOST = os.environ.get('DATABASE_HOST', 'DATABASE_HOST')
+DATABASE_PORT = os.environ.get('DATABASE_PORT', 'DATABASE_PORT')
 
 ## Firebase
 FIREBASE_CREDENTIAL_FILE = os.environ.get('FIREBASE_CREDENTIAL_FILE', "/app/rest/firebase-credential.json")
